092f800e75f6d1bc8c2c6649a10ad5e6dd082b97
himura <himurka@gmail.com>
2015-03-24 17:45:22 +0300

    ok

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,35 @@
 $(function(){
 $('.menutop a:eq(0)').bind('mouseover',function(){
-		$('.menutop').css('background','red')
+		$('.menutop [href="main"]').css('background','white')
 	})
 $('.menutop a:eq(0)').bind('mouseout',function(){
-		$('.menutop').css('background','')
+		$('.menutop [href="main"]').css('background','')
+	})
+});
+
+$(function(){
+$('.menutop a:eq(1)').bind('mouseover',function(){
+		$('.menutop [href="company"]').css('background','white')
+	})
+$('.menutop a:eq(1)').bind('mouseout',function(){
+		$('.menutop [href="company"]').css('background','')
+	})
+});
+
+$(function(){
+$('.menutop a:eq(2)').bind('mouseover',function(){
+		$('.menutop [href="stores"]').css('background','white')
+	})
+$('.menutop a:eq(2)').bind('mouseout',function(){
+		$('.menutop [href="stores"]').css('background','')
+	})
+});
+
+$(function(){
+$('.menutop a:eq(3)').bind('mouseover',function(){
+		$('.menutop [href="contact"]').css('background','white')
+	})
+$('.menutop a:eq(3)').bind('mouseout',function(){
+		$('.menutop [href="contact"]').css('background','')
 	})
 });
--- routes/gallery.js
+++ routes/gallery.js
@@ -1,5 +1,5 @@
 exports.index = function(req,res){
 	styles[2]='css/gallery.css'; //определяем стиль
-	scripts[3]='javascripts/main.js'; //определяем скрипт
+	//scripts[3]='javascripts/main.js'; //определяем скрипт
 	res.render('gallery');
 }
